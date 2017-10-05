d95a2f2ab97d159b2f94d86fb7c1084a86db5078
Dylan Lester <dylanclester@gmail.com>
2017-10-06 00:45:08 +1100

    Add minor interactivity to alarms

--- src/Alarm.js
+++ src/Alarm.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 
 class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.time,
+      status: props.status || "on"
+    };
+
+    this.handleStatusToggle = this.handleStatusToggle.bind(this);
+  }
+
+  handleStatusToggle() {
+    // TODO: Set in S3 bucket
+
+    const status = this.state.status === "on" ? "off" : "on";
+    this.setState({
+      status
+    });
+  }
+  
   render () {
+    const {time, status} = this.state;
     return (
       <div className="alarm">
-        <span className="time">{this.props.time}</span>
-        <span className={`status ${this.props.status}`}>{this.props.status}</span>
+        <span className="time">{time.format('h:mm a, dddd')}</span>
+        <span className={`status ${status}`} onClick={this.handleStatusToggle}>{status}</span>
       </div>
     );
   }
