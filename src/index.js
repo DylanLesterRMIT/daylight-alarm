ea472aa8a82dd8600387aafe4ebcc91e80b625dd
Dylan Lester <dylanclester@gmail.com>
2017-10-11 22:37:25 +1100

    Add AWS SDK for S3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
+import AWS from 'aws-sdk';
 
 import Clock from './Clock';
+
+AWS.config.update({
+  accessKeyId: 'AKIAIZJM2QUEPXT6JAHQ',
+  secretAccessKey: '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',
+  region: 'ap-southeast-2'
+});
 
 class App extends React.Component {
   render () {
