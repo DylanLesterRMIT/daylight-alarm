4ceb2fc10b9503a4589833d7bdce9ec4d3acb667
Dylan Lester <dylanclester@gmail.com>
2017-10-12 02:57:59 +1100

    Update create-alarms to make cloudwatchevents

--- lambda-fns/create-alarms.js
+++ lambda-fns/create-alarms.js
@@ -13,21 +13,61 @@ exports.handler = (event, context, callback) => {
         Key: key,
     };
     
-    s3.getObject(params, (err, data) => {
-        if (err) {
+    s3.getObject(params).promise()
+        .then(dataFile => {
+            const alarms = JSON.parse(dataFile.Body.toString());
+    
+            // Create CloudWatch events from alarms array
+            createCloudWatchRulesFromAlarms(alarms);
+        })
+        .catch(err => {
             console.log(err);
             const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
             console.log(message);
-            return;
-        }
-
-        const alarms = JSON.parse(data.Body.toString());
-        console.log('Alarms object:', alarms);
-
-        // TODO: Create CloudWatch events from alarms array
-        
-    });
+        });
 };
 
-// A lot of the above code has been sourced from the starter Lambda functions
-// provided by Amazon
+function createCloudWatchRulesFromAlarms(alarms) {
+    const cloudwatchevents = new aws.CloudWatchEvents();
+
+    // Iterate over alarms and create associated rules
+    alarms.forEach(alarm => {
+        // Extract alarm time
+        const date = new Date(alarm.time);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+
+        // Prepare parameters
+        const scheduleExpression = `cron(${minutes} ${hours} * * ? *)`; // Set alarm to run everyday
+        const params = {
+            Name: alarm.id,
+            ScheduleExpression: scheduleExpression
+        };
+    
+        // Create alarms
+        cloudwatchevents.putRule(params).promise()
+            .then(success => console.log(`Created rule for ${alarm.id} successfully!`))
+            .catch(err => console.log(err, err.stack));
+    });
+}
+
+// FIXME
+
+// The below is mainly pseudo code that states what should happen in createCloudWatchRulesFromAlarms.
+// Currently, it is unconcerned with the below.
+/*
+1. List all rules via cloudwatchevents.listRules(...)
+cloudwatchevents.listRules({}).promise()
+.then(rules => console.log('Rules:', rules))
+.catch(err => console.log(err, err.stack));
+
+2. Remove all rules that are not in the passed alarms using cloudwatchevents.deleteRule(...)
+  this may involve the use of cloudwatchevents.removeTargets(...)
+
+3. Add rules that have a status of "on" (using cloudwatchevents.putRule(...)) and disable 
+  rules that have status set to "off" (using cloudwatchevents.disableRule(...)).
+  This also means using cloudwatchevents.enableRule(...) to turn them back on
+
+
+--- may need to use cloudwatchevents.putTargets(...) to attach the Lambda function to them
+*/
